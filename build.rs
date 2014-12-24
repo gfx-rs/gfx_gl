b74a3e70a2d7689369de49bbf99aef7d25bac6a6
Armin Ronacher <armin.ronacher@active-4.com>
2014-12-24 22:53:08 +0100

    Added missing build file

---
+++ build.rs
@@ -0,0 +1,18 @@
+extern crate gl_generator;
+extern crate khronos_api;
+
+use std::os;
+use std::io::File;
+
+fn main() {
+    let dest = Path::new(os::getenv("OUT_DIR").unwrap());
+
+    let mut file = File::create(&dest.join("gl_bindings.rs")).unwrap();
+
+    gl_generator::generate_bindings(gl_generator::GlobalGenerator,
+                                                   gl_generator::registry::Ns::Gl,
+                                                   khronos_api::GL_XML,
+                                                   vec!["GL_EXT_texture_filter_anisotropic".to_string()],
+                                                   "4.5", "core",
+                                                   &mut file).unwrap();
+}
