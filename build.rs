735d489ff3863b847834529bff0c6a24baa850f0
Alex Butler <alexheretic@gmail.com>
2019-04-09 01:10:20 +0100

    Add s3tc extensions

--- build.rs
+++ build.rs
@@ -1,9 +1,7 @@
 extern crate gl_generator;
 
-use gl_generator::{Registry, Api, Profile, Fallbacks};
-use std::env;
-use std::fs::File;
-use std::path::Path;
+use gl_generator::{Api, Fallbacks, Profile, Registry};
+use std::{env, fs::File, path::Path};
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
@@ -12,11 +10,19 @@ fn main() {
 
     let mut file = File::create(&dest.join("gl_bindings.rs")).unwrap();
 
-    Registry::new(Api::Gl, (4, 6), Profile::Core, Fallbacks::All, [
-        "GL_EXT_texture_filter_anisotropic",
-        "GL_ARB_draw_buffers_blend",
-        "GL_ARB_program_interface_query",
-        ])
-        .write_bindings(gl_generator::StructGenerator, &mut file)
-        .unwrap();
+    Registry::new(
+        Api::Gl,
+        (4, 6),
+        Profile::Core,
+        Fallbacks::All,
+        [
+            "GL_EXT_texture_filter_anisotropic",
+            "GL_ARB_draw_buffers_blend",
+            "GL_ARB_program_interface_query",
+            "GL_EXT_texture_compression_s3tc",
+            "GL_EXT_texture_sRGB",
+        ],
+    )
+    .write_bindings(gl_generator::StructGenerator, &mut file)
+    .unwrap();
 }
